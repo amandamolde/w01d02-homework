678adfc152dfa9c88d9991cab332dc655ad033de
Amanda Molde <amanda.molde@gmail.com>
2018-06-12 21:02:16 -0600

    Commit 9 - Created loops.js file

---
+++ loops.js
@@ -0,0 +1,34 @@
+// for (x = 0; x <= 99; x++) {
+// 	console.log(x);
+// }
+
+// for (i = 99; i >= 0; i--) {
+// 	console.log(i);
+// }
+
+// for (a = 0; a <= 98; a++) {
+// 	if (a % 2 === 0) {
+// 		console.log(a);
+// 	}
+// }
+
+// for (b = 98; b >=0; b--) {
+// 	if (b % 2 === 0) {
+// 		console.log(b);
+// 	}
+// }
+
+// for (x = 49; x <= 72; x++) {
+// 	console.log(x);
+// }
+
+
+// for (x = 81; x >= 26; x--) {
+// 	console.log(x);
+// }
+
+for (x = 3; x <=90; x++) {
+	if (x % 3 === 0) {
+		console.log(x);
+	}
+}
